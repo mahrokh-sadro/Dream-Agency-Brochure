afdb868768ff736658ab763e5edf88ca562c59b1
AutoLogon <msadrolodabaee@myseneca.ca>
2021-06-21 11:09:32 -0400

    adding code for auto welcome email

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -10,28 +10,12 @@ router.post("/login", (req, res) => {
 
 router.post("/registration", (req, res) => {
 
-    const sgMail = require('@sendgrid/mail')
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-    
-    const msg = {
-      to: `${req.body.reg_email}`, // Change to your recipient
-      from: 'msadrolodabaee@myseneca.ca', // Change to your verified sender
-      subject: 'Sending with SendGrid is Fun',
-      text: 'and easy to do anywhere, even with Node.js',
-      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    }
-    
-    sgMail
-      .send(msg)
-      .then((response) => {
-        console.log(response[0].statusCode)
-        console.log(response[0].headers)
-      })
-      .catch((error) => {
-        console.error(error)
-      })
+   
 
-      
+
+
+    
+
     let name_err;
     let lastname_err;
     let email_err;
