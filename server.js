6339657660dc349bde1a09c4e2864fced6da5a11
AutoLogon <msadrolodabaee@myseneca.ca>
2021-06-27 06:58:52 -0400

    heroku

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use("/destination",destinationController);
 app.use("/user",userController);
 
 
-const PORT = 3000; 
+const PORT =process.env.PORT || 3000; 
 
 app.listen(PORT, ()=>{  // 2 Parameters: PORT, callback function
     console.log(`Web Server Application is running and listening on port ${PORT}`);
