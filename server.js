d38f89156cd44eca21e4f97e257539599ebae764
mahrokh sadro <msadrolodabaee@myseneca.ca>
2023-01-19 16:53:37 -0500

    final

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
 
 app.use("/", generalController);
 app.use("/destination", destinationController);
-const HTTP_PORT = 3000;
+const HTTP_PORT = process.env.PORT || 3000;
 
 app.listen(HTTP_PORT, () => {
   console.log(
